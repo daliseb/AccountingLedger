21c2b98c1202937f8ae782e42796c04bdbbbd244
daliseb <odaliseb59@gmail.com>
2025-10-10 12:58:12 -0400

    before getters and setters

--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Transactions {
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private Float amount;
+
+
+
 }
