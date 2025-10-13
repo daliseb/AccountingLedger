9ecf5c73596a07fea37317043ae1173e447fc4f3
daliseb <odaliseb59@gmail.com>
2025-10-13 10:09:32 -0400

    quick save

--- src/main/java/com/pluralsight/ConsoleHelper.java
+++ src/main/java/com/pluralsight/ConsoleHelper.java
@@ -7,7 +7,7 @@ public class ConsoleHelper {
     System.out.println("Please select one of the following options:");
 
     for (int i = 0; i < inventory.size(); i ++) {
-        Transactions t = inventory.get(i);
+        Transactions t = inventory.get(i);  //finishing what was on pg 13
     }
 
 
