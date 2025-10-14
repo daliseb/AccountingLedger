e3a44ab6e32ad337d759a61747fa4d324a73dd97
daliseb <odaliseb59@gmail.com>
2025-10-14 11:16:00 -0400

    add new things

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,6 +1,9 @@
 package com.pluralsight;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -12,17 +15,32 @@ public class Main {
 
             System.out.println(transactions.get(i).getTransactions);
         }
-
-
-
-
-
-    ArrayList<Transactions> transactions = getTransactions();
-
-
-
-
-
+        //ArrayList<Transactions> transactions = getTransactions();
+//we need to read the csv file, add filereader?
 
     }
-}
+        void getTransactions();
+    //public static void getTransactions(){
+        try{
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String input;
+
+            while((input = bufferedReader.readLine())!= null) {
+                System.out.println(input);
+
+            }
+            bufferedReader.close();
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
